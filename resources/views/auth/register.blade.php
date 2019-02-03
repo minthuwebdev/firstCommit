090494c3cb94c05463015eff064c9b965c20eb21
phpcrazy <phpcrazy@gmail.com>
2019-02-03 18:05:22 +0630

    Add Register Socialise

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,8 +81,8 @@
   <p>- OR -</p>
   <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign up using
   Facebook</a>
-  <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign up using
-  Google+</a>
+  <a href="{{ route('github-login', 'github') }}" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-github"></i> Sign up using
+  GitHub</a>
 </div>
 
 <a href="login" class="text-center">I already have a membership</a>
